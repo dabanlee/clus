d6aea185f96226f4ddedfb7f4a2e81810e314c21
Clear <576839360@qq.com>
2016-09-04 14:52:04 +0800

    ⬆️ update configure files

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,42 @@
-export const paths = {
+//
+// Rollup configure file
+//
+
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
+const paths = {
     root: '/',
     source: {
         root: './src/',
+        modules: './src/modules/',
     },
     dist: {
         root: './dist/',
     },
 };
 
-export default {
-	moduleName: 'Clus',
-	moduleId: 'Clus',
-    fileName: 'clus',
+let fileName,
+    Configure;
+
+fileName = process.env.NODE_ENV === 'development' ? 'clus' : 'clus.min';
+
+Configure = {
+    format: 'umd', // 'amd', 'cjs', 'es', 'iife', 'umd'
+    moduleName: 'Clus',
+    moduleId: 'Clus',
+    entry: `${paths.source.root}index.js`,
+    dest: `${paths.dist.root}${fileName}.js`,
+    sourceMap: true,
+    plugins: [
+        babel(),
+        sourcemaps(),
+    ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+    Configure.plugins.push(uglify());
 }
+
+export default Configure;
