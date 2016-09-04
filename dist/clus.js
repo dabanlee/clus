370dbdd5a33735216619709d255928c305c45bb7
Clear <576839360@qq.com>
2016-09-04 14:21:31 +0800

    ✨ add instance methods
    - each()
    - map()

--- dist/clus.js
+++ dist/clus.js
@@ -320,8 +320,23 @@ function is(selector) {
     return this.length > 0 && Clus.matches(this[0], selector);
 }
 
+function instanceMap(callback) {
+    return Clus(Clus.map(this, function (item, index) {
+        return callback.call(item, item, index);
+    }));
+}
+
+function instanceEach(callback) {
+    [].every.call(this, function (item, index) {
+        return callback.call(item, item, index) !== false;
+    });
+    return this;
+}
+
 Clus.fn.extend({
-    is: is
+    is: is,
+    map: instanceMap,
+    each: instanceEach
 });
 
 //
--- src/core/instance.js
+++ src/core/instance.js
@@ -6,6 +6,21 @@ export function is(selector) {
     return this.length > 0 && Clus.matches(this[0], selector);
 }
 
+export function instanceMap(callback) {
+    return Clus(Clus.map(this, function(item, index) {
+        return callback.call(item, item, index);
+    }));
+}
+
+export function instanceEach(callback) {
+    [].every.call(this, function(item, index) {
+        return callback.call(item, item, index) !== false;
+    });
+    return this;
+}
+
 Clus.fn.extend({
     is,
+    map: instanceMap,
+    each: instanceEach,
 });
